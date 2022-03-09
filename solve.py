bf5741fa1ca8a133c513487478215f328d47442a
FreeArcher <bespalov.alexey@yandex.ru>
2022-03-09 15:54:18 +0700

    made a programm what solve  fraction examples

---
+++ solve.py
@@ -0,0 +1,35 @@
+from fractions import Fraction as F
+import re
+
+#В видите вырожение
+#Пример может содержать дроби и целые числа с использованием знака "/" без пробела
+#Между знаками и числами должен быть обязательно пробел
+
+#ВЫРАЖЕНИЕ
+a = "3.5 + 2"
+
+#ПРОГРАММА
+l = re.findall('(\d+/\d+|\d+|\d+.\d+)\s([-*+:])', a)
+e = re.findall('(\d+/\d+|\d+|\d+.\d+)$', a)
+mainlist = l + e
+print(mainlist)
+
+s = ''
+for i in mainlist:
+    if isinstance(i, tuple):
+        val = i[0]
+        if i[1] == ':':
+            znak = '/'
+        else:
+            znak = i[1]
+
+    else:
+        val = i
+        znak = ''
+
+    s = s + f'F("{val}"){znak}'
+
+res = eval(s)
+print(f"{a} = {res}")
+print(f"{a} = {eval(str(res))}")
+
